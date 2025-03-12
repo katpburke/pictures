07244b19e88cc55d3fb4a7e1172105639243c39a
katpburke <katherinepackertburke@gmail.com>
2025-03-12 15:32:45 -0500

    downloadData test

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { downloadData } from 'aws-amplify/storage';
 import { useParams } from 'react-router-dom';
 
@@ -6,7 +6,27 @@ function Display() {
   const [img, updateImg] = useState(null);
   const { id } = useParams();
 
-  return (<div>Image #{id} will go here I promise</div>);
+  async function download() {
+    const result = await downloadData({
+      path: `upload/${id}`,
+    }).result;
+    updateImg(result);
+  }
+
+  useEffect(() => {
+    console.log('Download results: ', img);
+  }, [img]);
+
+  useEffect(() => {
+    download();
+  }, []);
+
+  return (
+    <div>
+      <div>Image #{id} will go here I promise</div>
+      {/* <div>{img}</div> */}
+    </div>
+  );
 }
 
 export default Display;
