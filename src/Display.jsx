2779453081408e1301655abbbb0e15d9fc5040a1
katpburke <katherinepackertburke@gmail.com>
2025-03-12 16:55:12 -0500

    trying using the StorageImage component

--- src/Display.jsx
+++ src/Display.jsx
@@ -1,31 +1,33 @@
 import { useState, useEffect } from 'react';
 import { downloadData } from 'aws-amplify/storage';
 import { useParams } from 'react-router-dom';
+import { StorageImage } from '@aws-amplify/ui-react-storage';
 
 function Display() {
-  const [img, updateImg] = useState(null);
+  //   const [img, updateImg] = useState(null);
   const { id } = useParams();
 
-  async function download() {
-    const { body } = await downloadData({
-      path: `upload/${id}`,
-    }).result;
-    updateImg(body);
-  }
+  //   async function download() {
+  //     const { body } = await downloadData({
+  //       path: `upload/${id}`,
+  //     }).result;
+  //     updateImg(body);
+  //   }
 
-  useEffect(() => {
-    console.log('Download results: ', img);
-  }, [img]);
+  //   useEffect(() => {
+  //     console.log('Download results: ', img);
+  //   }, [img]);
 
-  useEffect(() => {
-    download();
-  }, []);
+  //   useEffect(() => {
+  //     download();
+  //   }, []);
 
   return (
     <div>
       <div>Image #{id} will go here I promise</div>
       <div>
-        <img src={img} />
+        {/* <img src={img} /> */}
+        <StorageImage path={`upload/${id}`} />
       </div>
     </div>
   );
